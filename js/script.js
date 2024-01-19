79753ab6fce95f13ddd9a5e69a562914801d93b2
Fran Infante <infante.portero@gmail.com>
2024-01-19 09:35:19 +0100

    fixed responsiveness

--- js/script.js
+++ js/script.js
@@ -6,8 +6,21 @@ document.addEventListener('DOMContentLoaded', function () {
     menuButton.addEventListener('click', function () {
         navList.classList.toggle('show');
 
+        // Toggle the display property based on the presence of the 'show' class
         navItems.forEach(item => {
             item.style.display = navList.classList.contains('show') ? 'block' : 'none';
         });
+    });
+
+    // Check the window width on resize and remove the 'show' class if it's larger than 1120px
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 1120) {
+            navList.classList.remove('show');
+
+            // Ensure that list items are visible
+            navItems.forEach(item => {
+                item.style.display = 'inline-block';
+            });
+        }
     });
 });
